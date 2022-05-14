7bdfe142b681eeb794df1bb9f96fc7a1a9e7bf3a
eckrichard01 <eckrichard01@gmail.com>
2022-05-14 11:38:16 +0200

    iconok keszek

--- src/Control/BagMenuData.java
+++ src/Control/BagMenuData.java
@@ -1,17 +1,73 @@
 package Control;
 
-import Model.Item;
+import Model.*;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BagMenuData extends AbstractTableModel {
 
     List<Item> data;
+    List<ImageIcon> icons;
 
     public BagMenuData(ArrayList<Item> i){
         data = i;
+        icons = new ArrayList<ImageIcon>();
+        for(Item item : data){
+            if(item instanceof Axe){
+                try {
+                    ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/axe.png")));
+                    icons.add(icon);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(item instanceof Cape){
+                try {
+                    ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/cape.png")));
+                    icons.add(icon);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(item instanceof BonusBag){
+                try {
+                    ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/bag.png")));
+                    icons.add(icon);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(item instanceof Glove){
+                try {
+                    ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/gloves.png")));
+                    icons.add(icon);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(item instanceof Agent){
+                try {
+                    ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/cream.png")));
+                    icons.add(icon);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(item instanceof Material){
+                try {
+                    ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/material.png")));
+                    icons.add(icon);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
@@ -21,17 +77,26 @@ public class BagMenuData extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        return 1;
+        return 2;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        Item item = data.get(rowIndex);
-        return item.getName();
+        if(columnIndex == 1){
+            Item item = data.get(rowIndex);
+            return item.getName();
+        }
+        else {
+            ImageIcon icon = icons.get(rowIndex);
+            return icon;
+        }
     }
 
     public Class<?> getColumnClass(int columnIndex) {
-        return String.class;
+        switch (columnIndex){
+            case 0: return ImageIcon.class;
+            default: return String.class;
+        }
     }
 
     public boolean isCellEditable(int rowIndex, int columnIndex) {
--- src/Control/GeneticCodeMenuData.java
+++ src/Control/GeneticCodeMenuData.java
@@ -4,18 +4,30 @@ import Model.Agent;
 import Model.GeneticCode;
 import Model.Item;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GeneticCodeMenuData extends AbstractTableModel {
 
     List<Agent> data;
+    List<ImageIcon> icons;
 
     public GeneticCodeMenuData(ArrayList<GeneticCode> i){
        ArrayList<Agent> agents = new ArrayList<>();
+        icons = new ArrayList<ImageIcon>();
         for(GeneticCode codes : i){
             agents.add(codes.getAgent());
+            try {
+                ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/cream.png")));
+                icons.add(icon);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         data = agents;
     }
@@ -27,17 +39,26 @@ public class GeneticCodeMenuData extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        return 1;
+        return 2;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        Item item = data.get(rowIndex);
-        return item.getName();
+        if(columnIndex == 1){
+            Item item = data.get(rowIndex);
+            return item.getName();
+        }
+        else {
+            ImageIcon icon = icons.get(rowIndex);
+            return icon;
+        }
     }
 
     public Class<?> getColumnClass(int columnIndex) {
-        return String.class;
+        switch (columnIndex){
+            case 0: return ImageIcon.class;
+            default: return String.class;
+        }
     }
 
     public boolean isCellEditable(int rowIndex, int columnIndex) {
